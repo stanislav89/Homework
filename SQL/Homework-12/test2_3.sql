eedb0541eaf4cd053c0edc7677cdb65deb676550
stanislav89 <50412853+stanislav89@users.noreply.github.com>
2020-11-22 18:55:49 +0200

    Add files via upload

---
+++ SQL/Homework-12/test2_3.sql
@@ -0,0 +1,50 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.0
+-- Dumped by pg_dump version 13.0
+
+-- Started on 2020-11-22 18:55:29
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE test2;
+--
+-- TOC entry 2979 (class 1262 OID 16685)
+-- Name: test2; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE test2 WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'Russian_Russia.1251';
+
+
+ALTER DATABASE test2 OWNER TO postgres;
+
+\connect test2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- Completed on 2020-11-22 18:55:29
+
+--
+-- PostgreSQL database dump complete
+--
+
